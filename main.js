e7bde68f7c4f03514e9f2ca6a0bc2ab09908c63b
brian-crypto <britomflor066@gmail.com>
2021-01-04 14:46:51 -0300

    responsive

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
         e.preventDefault();
         let target = e.currentTarget.getAttribute('href');
-
         document.querySelector(target).scrollIntoView({
             behavior: 'smooth'
         });
